ca2ef65f4b1acbb5860c7c8de5afa6790e806ef5
Denis Blondeau <dbappdev@outlook.com>
2020-10-05 19:01:54 -0400

    Fix.

--- Sources/CircularGaugeProgressViewStyle/CircularGaugeProgressViewStyle.swift
+++ Sources/CircularGaugeProgressViewStyle/CircularGaugeProgressViewStyle.swift
@@ -35,8 +35,8 @@ public struct CircularGaugeProgressViewStyle: ProgressViewStyle {
                 .stroke(strokeColor, style: StrokeStyle(lineWidth: CGFloat(strokeWidth), lineCap: .round))
 
             Text(percentage)
-                .font(.system(size: 50))
-                .bold()
+                .font(.system(size: 35, weight: .bold, design: .rounded))
+                .foregroundColor(.white)
                 .offset(y: -4)
                 .animation(nil)
         }
